15356a7196547be909fbb3e555c0d9b6866b64f6
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-25 05:34:26 +0600

    Primitive and non-primitive data type

--- types.js
+++ types.js
@@ -6,9 +6,28 @@
 // int [] numbers = [12, 45, 78];
 // string[] friends = ['abul', 'bablul'];
 
-// dynamic tpe Language;
+// JavaScript is a dynamic type programming language;
+
+// primitive type
 const a = 5;
 const b = 'Samsu kopai na ekhon ar';
+const d = true;
+
+
+// non-primitive
 const ages = [45, 65, 48];
-const student = { id: 23, class: 7 }
-console.log(typeof a, typeof b, typeof ages, typeof student);
+const student = { id: 23, class: 7 };
+
+// console.log(typeof a, typeof b, typeof d, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = { job: 'web developer' };
+let q = p;
+q.job = 'front end developer';
+// q = { job: 'Backend end' };
+console.log(p, q);
