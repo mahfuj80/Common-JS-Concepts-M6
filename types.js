39b966a212e1fa45f371256ccd85d0b5a27b8e8d
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-24 23:11:15 +0600

    initial commit

---
+++ types.js
@@ -0,0 +1,14 @@
+// strongly typed Language
+// int a = 5;
+// string b = 'alim halim khalim';
+// bool c = True;
+// object student = {name: 'noya daman', id: 55}
+// int [] numbers = [12, 45, 78];
+// string[] friends = ['abul', 'bablul'];
+
+// dynamic tpe Language;
+const a = 5;
+const b = 'Samsu kopai na ekhon ar';
+const ages = [45, 65, 48];
+const student = { id: 23, class: 7 }
+console.log(typeof a, typeof b, typeof ages, typeof student);
