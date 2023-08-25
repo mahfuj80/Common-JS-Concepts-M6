42128871bd7ac38d15287e3e8ba362370168d5ae
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-25 08:39:30 +0600

    function arguments pass by reference pass by value

---
+++ arguments.js
@@ -0,0 +1,13 @@
+// arguments array like object;
+function sum(a, b, c) {
+    const args = [...arguments];
+    // console.log(args);
+    // console.log(typeof arguments);
+    const result = a + b + c;
+    return result;
+}
+// console.log(arguments);
+const total = sum(45, 89, 12, 45, 98, 56);
+// console.log(total);
+// console.log(typeof sum);
+console.log(sum.length);
